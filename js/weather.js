d5e2c2a964ff7e80e8a221d4fe579df97bb5282f
MotazDegmis <72710619+MotazDegmis@users.noreply.github.com>
2021-11-29 22:55:24 +0300

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -48,6 +48,7 @@ export default class WEATHER {
             this.collectData(data) //Pass data to function for processing
         }catch(er){
             console.log(er)
+            
         }
         
     }
